13edf8987d5995ba16d31037444ec753613520bc
TheRealMilesLee <grandtheftauto710@outlook.com>
2024-10-27 21:39:41 -0700

    Update the graph 2, move labels inside and add a onHover expand the slice effect

--- Homework3/trdli/src/VisualizeLayout.js
+++ Homework3/trdli/src/VisualizeLayout.js
@@ -448,7 +448,7 @@ function Graph1_Overall()
           .duration(500)
           .ease(d3.easeLinear)
           .style("stroke", lineData =>
-            filteredData.includes(lineData) ? "#781aeb" : "#404d43"
+            filteredData.includes(lineData) ? "#0384fc" : "#404d43"
           )
           .style("opacity", lineData =>
             filteredData.includes(lineData) ? 1 : 0.1
@@ -470,16 +470,12 @@ export function mountChart2()
 }
 
 /**
- * @brief The function `Graph2_data_cleaning` filters out non-luxury car brands from a dataset based on a predefined list of luxury brands.
- * @returns The function `Graph2_data_cleaning()` is returning an array of
- * objects where each object contains the `make` property from the
- * `column_from_csv` array after converting it to lowercase. The function
- * filters out any entries that do not belong to the luxury car brands listed
- * in the `luxuryBrands` array.
+ * @brief The function `Graph2_data_cleaning` filters out non-luxury car brands from a dataset based on a predefined list of luxury brands and groups the data by car maker, counting the number of cars sold.
+ * @returns {Object} An object containing the filtered data array and the total count of cars sold for non-luxury brands.
  */
 function Graph2_data_cleaning()
 {
-  return column_from_csv.map(d =>
+  const cleanedData = column_from_csv.map(d =>
   {
     const make = d.make.toLowerCase();
     const luxuryBrands = ['ferrari', 'rolls-royce', 'fisker', 'tesla',
@@ -494,6 +490,31 @@ function Graph2_data_cleaning()
     }
     return null;
   }).filter(d => d !== null);  // Filter out null entries
+
+  // Group the data by car maker and count the number of cars sold
+  const carMakerCount = d3.rollup(cleanedData, v => v.length, d => d.make);
+  // Convert the Map to an array for pie chart
+  const data = Array.from(carMakerCount, ([make, count]) => ({ make, count }));
+
+  // Merge the low percentage makers (percentage < 2.5) into 'Other'
+  let total = d3.sum(data, d => d.count);
+  let otherCount = 0;
+  const filteredData = data.filter(d =>
+  {
+    if (d.count / total * 100 < 2.5)
+    {
+      otherCount += d.count;
+      return false;
+    }
+    return true;
+  });
+
+  if (otherCount > 0)
+  {
+    filteredData.push({ make: 'Other', count: otherCount });
+  }
+
+  return { filteredData, total };
 }
 
 /**
@@ -518,48 +539,29 @@ function Graph2_Detail()
     .append("g")
     .attr("transform", `translate(${ width / 2 }, ${ height / 2 })`);
 
-  // Data preprocessing, group the data by car maker and count the number of
-  // cars sold
-  const carMakerCount = d3.rollup(Graph2_data_cleaning(), v => v.length,
-    d => d.make);
-  // Convert the Map to an array for pie chart
-  const data = Array.from(carMakerCount, ([make, count]) =>
-    ({ make, count }));
+  // Clean the data and get the filtered data and total number of car make
+  const recieved_clean_result = Graph2_data_cleaning();
+  const filteredData = recieved_clean_result.filteredData;
+  const total_number_of_car_make = recieved_clean_result.total;
 
-  // Merge the low percentage makers (percentage < 3) into 'Other'
-  let total = d3.sum(data, d => d.count);
-  let otherCount = 0;
-  const filteredData = data.filter(d =>
-  {
-    if (d.count / total * 100 < 2.5)
-    {
-      otherCount += d.count;
-      return false;
-    }
-    return true;
-  });
-
-  if (otherCount > 0)
-  {
-    filteredData.push({ make: 'Other', count: otherCount });
-  }
+  /** Data cleaning finished, now we can start to draw the pie chart */
 
   // Config the size of the pie
-  const radius = Math.min(width, height) / 3;
+  const radius = (Math.min(width, height) / 3.1415926) + 50;
 
-  // Set up the color scale
+  // Set up the color scale based on the car maker
   const color = d3.scaleOrdinal()
     .domain(filteredData.map(d => d.make))
     .range(d3.schemeCategory10);
 
-  // Create the pie layout
-  const pie = d3.pie()
-    .value(d => d.count);
+  // Create the pie layout, value is the count of cars sold
+  const pie = d3.pie().value(d => d.count);
 
-  // Create the arc generator
-  const arc = d3.arc()
-    .innerRadius(0)
-    .outerRadius(radius);
+  // Create the arc generator, innerRadius is 5, outerRadius is radius
+  const arc = d3.arc().innerRadius(5).outerRadius(radius);
+
+  // Create the arc generator for hover effect, it with expand the arc by 1.15
+  const arcHover = d3.arc().innerRadius(5).outerRadius(radius * 1.15);
 
   // Bind data and create pie chart slices
   const arcs = chartContainer_graph2.selectAll(".arc")
@@ -568,69 +570,78 @@ function Graph2_Detail()
     .append("g")
     .attr("class", "arc");
 
+  // Create the hover effect for the slices
   arcs.append("path")
     .attr("d", arc)
-    .attr("fill", d => color(d.data.make));
+    .attr("fill", d => color(d.data.make))
+    .on("mouseover", function (event, d) // Add hover effect
+    {
+      d3.select(this).transition()
+        .duration(100) // Set the duration of the transition
+        .attr("d", arcHover);
 
-  // Create the arc for the labels
-  const outerArc = d3.arc()
-    .outerRadius(radius * 1.2)
-    .innerRadius(radius * 1.2);
+      // Enlarge the corresponding text label
+      chartContainer_graph2.selectAll("text")
+        .filter(textData => textData.data.make === d.data.make)
+        .transition()
+        .duration(100)
+        .style("font-size", "14px");
+    })
+    .on("mouseout", function (event, d)
+    {
+      d3.select(this).transition()
+        .duration(100) // Set the duration of the transition
+        .attr("d", arc);
 
-  // Create text labels outside the pie
+      // Reset the corresponding text label size
+      chartContainer_graph2.selectAll("text")
+        .filter(textData => textData.data.make === d.data.make)
+        .transition()
+        .duration(100)
+        .style("font-size", "10px");
+    });
+
+  // Create text labels, this is inside of the pie chart, the label would rotate to fit into the slices
   chartContainer_graph2.selectAll("text")
     .data(pie(filteredData))
     .enter()
     .append("text")
     .attr("transform", function (d)
     {
-      const pos = outerArc.centroid(d);
-      const mid = midAngle(d);
-      // Adjust for left/right
-      pos[0] = radius * (mid < Math.PI ? 1.3 : -1.3);
-
-      // Adjust 'volkswagen' label manually
-      if (d.data.make === "volkswagen")
-      {
-        pos[1] -= 20;  // Move up by 20 units
-      }
-
-      return `translate(${ pos })`;
+      const pos = arc.centroid(d);
+      const angle = midAngle(d) * 180 / Math.PI - 90;
+      pos[0] = pos[0] * 1.3; // Move the label slightly outwards
+      pos[1] = pos[1] * 1.3; // Move the label slightly outwards
+      return `translate(${ pos }) rotate(${ angle })`;
     })
-    .attr("text-anchor", function (d)
-    {
-      return (midAngle(d) < Math.PI ? "start" : "end");
-    })
+    .attr("dy", ".35em")
+    .attr("text-anchor", "middle")
     .text(d =>
     {
-      const percentage = ((d.data.count / total) * 100).toFixed(2);
+      const percentage = ((d.data.count / total_number_of_car_make) * 100).toFixed(2);
       return `${ d.data.make } (${ percentage }%)`;
+    })
+    .style("font-size", "10px")
+    .style("font-weight", "bold")
+    .style("fill", "black")
+    .attr("transform", function (d)
+    {
+      const pos = arc.centroid(d);
+      const angle = midAngle(d) * 180 / Math.PI - 90;
+      pos[0] = pos[0] * 1.3; // Move the label slightly outwards
+      pos[1] = pos[1] * 1.3; // Move the label slightly outwards
+      const rotation = (['toyota', 'dodge', 'chrysler', 'jeep', 'infiniti', 'hyundai', 'honda', 'kia', 'nissan', 'volksvagen'].includes(d.data.make)) ? angle + 180 : angle;
+      return `translate(${ pos }) rotate(${ rotation })`;
     });
 
-  // Create the lines connecting slices to labels
-  chartContainer_graph2.selectAll("polyline")
-    .data(pie(filteredData))
-    .enter()
-    .append("polyline")
-    .attr("points", function (d)
-    {
-      const pos = outerArc.centroid(d);
-      const mid = midAngle(d);
-      const outerPos = pos.slice();
-      // Position of label
-      outerPos[0] = radius * (mid < Math.PI ? 1.3 : -1.3);
-      // Adjust 'volkswagen' polyline position manually other wise it would
-      // collide
-      if (d.data.make === "volkswagen")
-      {
-        outerPos[1] -= 20;  // Move line connection up by 20 units
-      }
-      return [arc.centroid(d), outerArc.centroid(d), outerPos];
-    })
-    .style("fill", "none")
-    .style("stroke", "black");
-
-  // MidAngle function for determining side of the pie
+  /**
+   * Calculates the midpoint angle of a given arc.
+   *
+   * @param {Object} d - An object representing the arc.
+   * @param {number} d.startAngle - The starting angle of the arc.
+   * @param {number} d.endAngle - The ending angle of the arc.
+   * @returns {number} The midpoint angle of the arc.
+   */
   function midAngle(d)
   {
     return d.startAngle + (d.endAngle - d.startAngle) / 2;
